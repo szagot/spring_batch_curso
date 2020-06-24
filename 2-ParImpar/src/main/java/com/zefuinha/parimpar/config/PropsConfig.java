f96d04c770837b5a74e641849a9f635ad01b47f8
Daniel Bispo <szagot@gmail.com>
2020-06-24 17:23:36 -0300

    Geral: Externalizando arquivos de configuração

---
+++ 2-ParImpar/src/main/java/com/zefuinha/parimpar/config/PropsConfig.java
@@ -0,0 +1,25 @@
+package com.zefuinha.parimpar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * Externaliza o arquivo de configuração (application.properties)
+ */
+
+@Configuration
+public class PropsConfig {
+
+	@Bean
+	public PropertySourcesPlaceholderConfigurer config() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		
+		// Define onde está o arquivo de configuração do Batch
+		configurer.setLocation(new FileSystemResource("..\\application.properties"));
+		
+		return configurer;
+	}
+	
+}
