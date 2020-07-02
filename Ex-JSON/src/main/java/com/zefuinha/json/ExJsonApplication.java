bedbfab13392acb66032e61b72c2e5601129a236
Daniel Bispo <szagot@gmail.com>
2020-07-02 09:17:47 -0300

    Exercicio: Testando batch com arquivo JSON - Base

---
+++ Ex-JSON/src/main/java/com/zefuinha/json/ExJsonApplication.java
@@ -0,0 +1,13 @@
+package com.zefuinha.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExJsonApplication.class, args);
+	}
+
+}
---
+++ Ex-JSON/src/test/java/com/zefuinha/json/ExJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zefuinha.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
