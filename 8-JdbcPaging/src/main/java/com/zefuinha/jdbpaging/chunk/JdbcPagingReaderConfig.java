d0c545047775ac133acd77c381657982b046078b
Daniel Bispo <szagot@gmail.com>
2020-07-08 16:12:08 -0300

    JDBC Paging: Criado reader com paginador

--- 8-JdbcPaging/src/main/java/com/zefuinha/jdbpaging/chunk/JdbcPagingReaderConfig.java
+++ 8-JdbcPaging/src/main/java/com/zefuinha/jdbpaging/chunk/JdbcPagingReaderConfig.java
@@ -1,8 +1,15 @@
 package com.zefuinha.jdbpaging.chunk;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.zefuinha.jdbpaging.dominio.Cliente;
 
@@ -10,8 +17,45 @@ import com.zefuinha.jdbpaging.dominio.Cliente;
 public class JdbcPagingReaderConfig {
 
 	@Bean
-	public JdbcPagingItemReader<Cliente> jdbcPagingReader() {
-		return null;
+	public JdbcPagingItemReader<Cliente> jdbcPagingReader(
+			// Banco da Aplicação
+			@Qualifier("appDataSource")
+			DataSource dataSource,
+			// Injeção do gerador de query para o paginador
+			PagingQueryProvider queryProvider
+	) {
+		return new JdbcPagingItemReaderBuilder<Cliente>()
+				// Nome
+				.name("jdbcPagingReader")
+				// BD
+				.dataSource(dataSource)
+				// Gerador de consulta injetado
+				.queryProvider(queryProvider)
+				// Tamanho da página
+				.pageSize(1)
+				// Mapeando as linhas no objeto Cliente
+				.rowMapper(new BeanPropertyRowMapper<Cliente>(Cliente.class))
+				// Gerando leitor
+				.build();
+	}
+	
+	@Bean
+	public SqlPagingQueryProviderFactoryBean queryProvider(
+			// Banco da Aplicação
+			@Qualifier("appDataSource")
+			DataSource dataSource
+	) {
+		SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+		// BD
+		queryProvider.setDataSource(dataSource);
+		// Area do select
+		queryProvider.setSelectClause("select *");
+		// Area do from
+		queryProvider.setFromClause("from cliente");
+		// Chave de ordenação (para garantir integridade do retorno em cada página)
+		queryProvider.setSortKey("email");
+		
+		return queryProvider;
 	}
 	
 }
